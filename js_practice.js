85de4c596c4e41548fa33fefd535df8e0ec583b6
Roman <romanvasylyna888@gmail.com>
2020-05-15 21:00:12 +0300

    Продвинулся в калькуляторе

--- js_practice.js
+++ js_practice.js
@@ -1,15 +1,92 @@
-let input = document.querySelector("input");
-let calcTopDisplay = document.querySelector(".calculator-display-top");
-let calcBottomDisplay = document.querySelector(".calculator-display-bottom");
-input.addEventListener('keypress', fillDisplay);
+//Getting Elements
+let calcDisplayTop = document.querySelector('.calculator-display-top');
+let calcDisplayBottom = document.querySelector('.calculator-display-bottom');
+let buttons = document.querySelectorAll('.btn');
 
 
-function fillDisplay(){
-if(event.keyCode == 13) {
-calcBottomDisplay.innerHTML = this.value;
-calcTopDisplay.innerHTML = this.value;
+//Looping trough each btn and adding event listener
+function addOnclickEvent(buttons){
+for(let i = 0; i < buttons.length; i++) {
+  switch(buttons[i].innerHTML) {
+    case "AC":
+    buttons[i].addEventListener('click', clearInput);
+    break;
+    case "DEL":
+    buttons[i].addEventListener('click', clearOneInputElem);
+    break;
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case ",":
+    buttons[i].addEventListener('click', displayNumber);
+    break;
+    case "+":
+    buttons[i].addEventListener('click', calculateNum);
+    break;
+    case "-":
+    buttons[i].addEventListener('click', calculateNum);
+    break;
+    case "&#247;":
+    buttons[i].addEventListener('click', calculateNum);
+    break;
+    case "*":
+    buttons[i].addEventListener('click', calculateNum);
+    break;
+
+
+  }
 }
 }
+
+//Output numbers 0-9 and ","
+function displayNumber() {
+checkIfZero();
+if(forbidSecondComma()){
+document.querySelector(".comma").removeEventListener('click', displayNumber);
+}
+calcDisplayBottom.innerHTML += this.innerHTML;
+}
+
+//Clearing only one element at a time
+function clearOneInputElem() {
+
+}
+
+//Math operations func
+function calculateNum() {
+calcDisplayTop.innerHTML = calcDisplayBottom.innerHTML + this.innerHTML;
+}
+
+
+//If display contains zero remove it
+function checkIfZero() {
+if(calcDisplayBottom.innerHTML == 0){
+calcDisplayBottom.innerHTML = '';
+}
+}
+
+//If display already contains one "," forbid others
+function forbidSecondComma() {
+let test = /\W/.test(calcDisplayBottom.innerHTML);
+return test;
+}
+
+//Completely clearing input
+function clearInput() {
+calcDisplayTop.innerHTML = '';
+calcDisplayBottom.innerHTML = 0;
+}
+
+//Activating function
+addOnclickEvent(buttons);
+
 
 
 
